3f3020bc87444f6197405ece2f0babd06b71645d
Gerald Bustamante <gerald.bustamante@usjr.edu.ph>
2021-09-07 21:26:10 +0800

    practice no.2

--- 2_JS/VariableType.js
+++ 2_JS/VariableType.js
@@ -3,11 +3,12 @@ var a = [1, 2, 3, 4, 5, 6];
 var b = { one: 1, "two": "two", three: [3] };
 var c;
 var d = null;
+var e = ("CUTE");
+var f = number;
 
 console.log(typeof number)
 console.log(`the variable ${number} is of type ${typeof number}`);
-console.log(b);
-b.four = "HI ALL";
-console.log(b);
-b.five = "hahhhahahaha";
-console.log(b);
+console.log(`I am ${d}`);
+console.log(`the var ${e} is of type ${typeof e}`)
+console.log(`I have no meaning there for I'm ${typeof c}`);
+console.log(`the variable b is an ${typeof b}`);
