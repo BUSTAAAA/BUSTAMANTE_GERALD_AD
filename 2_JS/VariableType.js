82822ed24dc22c523fb2adccb9762a6297d75e55
Gerald Bustamante <gerald.bustamante@usjr.edu.ph>
2021-09-07 15:58:24 +0800

    Practice

---
+++ 2_JS/VariableType.js
@@ -0,0 +1,13 @@
+var number = 1;
+var a = [1, 2, 3, 4, 5, 6];
+var b = { one: 1, "two": "two", three: [3] };
+var c;
+var d = null;
+
+console.log(typeof number)
+console.log(`the variable ${number} is of type ${typeof number}`);
+console.log(b);
+b.four = "HI ALL";
+console.log(b);
+b.five = "hahhhahahaha";
+console.log(b);
